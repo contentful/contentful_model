8e4ee37b2bf87966ea0d6a865c0002b0ef93a842
Ed Jones <ed@errorstudio.co.uk>
2014-12-07 16:19:03 +0000

    addition of the subclass's content type into the client configuration now handled in the inherited() method; added a descendents() method mostly for debugging autoload stuff

--- lib/contentful_model/base.rb
+++ lib/contentful_model/base.rb
@@ -6,11 +6,12 @@ module ContentfulModel
       super
       self.class.coercions ||= {}
       fields.each do |k,v|
-        self.class.send(:attr_accessor, k)
+        field_name = k.to_s.underscore
+        self.class.send(:attr_accessor, field_name)
         if self.class.coercions[k].nil?
-          instance_variable_set(:"@#{k}", v)
+          instance_variable_set(:"@#{field_name}", v)
         else
-          instance_variable_set(:"@#{k}", self.class::COERCIONS[self.class.coercions[k]].call(v))
+          instance_variable_set(:"@#{field_name}", self.class::COERCIONS[self.class.coercions[k]].call(v))
         end
       end
     end
@@ -18,11 +19,17 @@ module ContentfulModel
     class << self
       attr_accessor :content_type_id, :coercions
 
-      def client
+      def inherited(subclass)
         unless ContentfulModel.configuration.entry_mapping.has_key?(@content_type_id)
-          ContentfulModel.configuration.entry_mapping[@content_type_id] = Object.const_get(self.to_s.to_sym)
+          ContentfulModel.configuration.entry_mapping[@content_type_id] = Object.const_get(subclass.to_s.to_sym)
         end
+      end
 
+      def descendents
+        ObjectSpace.each_object(Class).select { |klass| klass < self }
+      end
+
+      def client
         @@client ||= Contentful::Client.new(ContentfulModel.configuration.to_hash)
       end
 
