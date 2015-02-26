3e4f157f0f72bf40d5fd1a73ff944837e144b8ea
Ed Jones <ed@errorstudio.co.uk>
2015-02-26 08:19:33 +0000

    Improve has_one and has_many associations - make sure the method exists on the child

--- lib/contentful_model/associations.rb
+++ lib/contentful_model/associations.rb
@@ -15,16 +15,15 @@ module ContentfulModel
       # class Foo
       #   has_many :bars
       # end
-      # TODO this breaks down in situations where the has_many end doesn't respond to
-      # bars because the association is really the other way around
+      # TODO this breaks down in situations where the has_many end doesn't respond to bars because the association is really the other way around
       # @param classname [Symbol] the name of the child model, as a plural symbol
-      def has_many(classname)
+      def has_many(classname, *opts)
         #define an instance method called the same as the arg passed in
         #e.g. bars()
         define_method "#{classname}" do
           # call bars() on super, and for each, call bar=(self)
           super().collect do |instance|
-            instance.send(:"#{self.class.to_s.singularize.camelize(:lower)}=",self)
+            instance.send(:"#{self.class.to_s.singularize.underscore}=",self)
             #return the instance to the collect() method
             instance
           end
@@ -32,19 +31,21 @@ module ContentfulModel
       end
 
       # has_one is called on the parent model, and sets a single instance var on the child
-      # it's conceptually identical to `has_many()`
       # which is named the singular of the class this module is mixed into
-      def has_one(classname)
+      # it's conceptually identical to `has_many()`
+      def has_one(classname, *opts)
         define_method "#{classname}" do
-          super().send(:"#{self.class.to_s.singularize.camelize(:lower)}=",self)
-          instance
+          if super().respond_to?(:"#{self.class.to_s.singularize.underscore}=")
+            super().send(:"#{self.class.to_s.singularize.underscore}=",self)
+          end
+          super()
         end
       end
 
 
       # belongs_to is called on the child, and creates methods for mapping to the parent
       # @param classname [Symbol] the singular name of the parent
-      def belongs_to(classname)
+      def belongs_to(classname, *opts)
         raise ArgumentError, "belongs_to requires a class name as a symbol" unless classname.is_a?(Symbol)
         define_method "#{classname}" do
           #this is where we need to return the parent class
