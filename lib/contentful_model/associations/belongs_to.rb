f23697ac7855d27c133c455481cb40d9002b2f69
Ed Jones <ed@errorstudio.co.uk>
2015-02-26 15:42:58 +0000

    belongs_to makes no sense in Contentful; raise NotImplementedError

--- lib/contentful_model/associations/belongs_to.rb
+++ lib/contentful_model/associations/belongs_to.rb
@@ -7,18 +7,9 @@ module ContentfulModel
 
       module ClassMethods
         # belongs_to is called on the child, and creates methods for mapping to the parent
-        # @param classname [Symbol] the singular name of the parent
-        def belongs_to(classname, *opts)
-          raise ArgumentError, "belongs_to requires a class name as a symbol" unless classname.is_a?(Symbol)
-          define_method "#{classname}" do
-            #this is where we need to return the parent class
-            self.instance_variable_get(:"@#{classname}")
-          end
-
-          define_method "#{classname}=" do |instance|
-            #this is where we need to set the class name
-            self.instance_variable_set(:"@#{classname}",instance)
-          end
+        # @param association_name [Symbol] the singular name of the parent
+        def belongs_to(association_name, opts = {})
+          raise NotImplementedError, "Contentful doesn't have a singular belongs_to relationship. Use belongs_to_many instead."
         end
       end
     end
