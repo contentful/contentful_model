a8fa929eb3adf3c76df596bce9e582f3b710c1ed
Ed Jones <ed@errorstudio.co.uk>
2015-03-31 16:29:05 +0100

    Pass the modified configuration to super() in ContentfulModel::Client

--- lib/contentful_model/client.rb
+++ lib/contentful_model/client.rb
@@ -1,14 +1,14 @@
 module ContentfulModel
   class Client < Contentful::Client
 
-    PREVIEW_API_URL = "https://preview.contentful.com"
+    PREVIEW_API_URL = "preview.contentful.com"
 
     def initialize(configuration)
       if ContentfulModel.use_preview_api
         configuration[:api_url] = PREVIEW_API_URL
         configuration[:access_token] = configuration[:preview_access_token]
       end
-      super
+      super(configuration)
     end
   end
 end
